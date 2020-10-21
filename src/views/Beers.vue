5ce9101fe1f47d1d0ce7d0ce32f2c5a710aad3ab
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-21 19:47:37 +0100

    adds beers view

---
+++ src/views/Beers.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for='beer in beers' :key='beer.name'>
+      <Beer :beer='beer'/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Beer from '@/components/Beer.vue'
+import { mapState } from 'vuex' 
+
+export default {
+  name: 'Beers',
+  components: {
+    Beer
+  },
+  computed: mapState(['beers'])
+}
+</script>
