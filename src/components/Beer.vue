668171cec791aa994c95dd2928274300262a7efc
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-21 20:43:56 +0100

    adds styling

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -1,8 +1,25 @@
 <template>
-    <div>
-        {{beer.name}}
-    </div>
+    <v-container style='background: pink'>
+        <v-row style='background: red'>
+            <v-col style='background: lime' justify="center">
+                <b>{{beer.name}}</b>
+                <p>{{beer.tagline}}</p>
+                {{beer}}
+            </v-col>
+            <v-col style='background: blue' justify="center">
+                <v-img max-width='2rem' :src='beer.image_url'></v-img>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+
+<script>
+export default {
+  name: 'Beer',
+  props: ['beer']
+}
+</script>
+
 
 <script>
 export default {
