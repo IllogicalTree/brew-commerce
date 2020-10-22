47438cbf5e53a54d394b5eb677b5793f0d7ae1a2
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-22 10:12:38 +0100

    adds legal disclaimer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Home page</h1>
+    <p>This is a non-commercial website developed as part of coursework at the University of the Highlands and Islands, to allow student teams to experiment with building websites based on open APIs </p>
   </div>
 </template>
 
