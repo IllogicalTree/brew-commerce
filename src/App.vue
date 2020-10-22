d6bd9b929df1531be9682da8cd599307185a419f
CALUM MACINTOSH <20014543@uhi.ac.uk>
2020-10-22 17:40:00 +0100

    add <v-main> tags
    and route-rview

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,23 @@
       dark
     >
       <div class="d-flex align-center">
-        <!-- <v-img
+        <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
-        /> -->
+        />
 
-        <!-- <v-img
+        <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        /> -->
+        />
       </div>
 
       <v-spacer></v-spacer>
@@ -49,7 +49,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/beers">Beers</router-link>
     </div>
-    <router-view/>
+    
+    <v-main> <router-view>  <router-view/>  </v-main> 
   </v-app>
 </template>
 
