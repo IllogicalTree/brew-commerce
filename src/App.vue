868560f35db8f89eeb8c30fb1bc3d35b852c671f
CALUM MACINTOSH <20014543@uhi.ac.uk>
2020-10-22 13:47:11 +0100

    Add a toolbar at top of page
    Navigation toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,50 @@
 <template>
   <v-app id="app">
+    <v-app-bar
+      app
+      color="grey darken-3"
+      dark
+    >
+      <div class="d-flex align-center">
+        <!-- <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        /> -->
+
+        <!-- <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        /> -->
+      </div>
+
+      <v-spacer></v-spacer>
+
+       <v-btn
+        href=""
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Products</span>
+        <v-icon>mdi-beer</v-icon>
+      </v-btn>
+
+      <v-btn
+        href=""
+        target="Beers"
+        text
+      >
+        <span class="mr-2">View Cart</span>
+        <v-icon>mdi-cart-outline</v-icon>
+      </v-btn>
+    </v-app-bar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/beers">Beers</router-link>
