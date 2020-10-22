d31a89998e0055de02f398f6db701dd55d7669ca
CALUM MACINTOSH <20014543@uhi.ac.uk>
2020-10-22 21:50:53 +0100

    Add 'Add to cart' button
    For each product - non functional

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -4,6 +4,7 @@
             <v-col style='background: lime' justify="center">
                 <b>{{beer.id}} - {{beer.name}}</b>
                 <p>{{beer.tagline}}</p>
+                <v-btn id="addToCart" x-large outlined color="orange" dark>Add to cart </v-btn>
             </v-col>
             <v-col style='background: blue' justify="center">
                 <v-img max-width='2rem' :src='beer.image_url'></v-img>
