b9e6df6ee81d89f1cdc18f2a3bc642f851b0ff6a
CALUM MACINTOSH <20014543@uhi.ac.uk>
2020-10-22 18:37:48 +0100

    change filepath to logo from cdn to assets

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          src="../assets/logo.png"
           width="100"
         />
       </div>
