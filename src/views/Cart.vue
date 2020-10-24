f79c84083526e23617d4e14a42d685b72e69c9ea
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-24 22:54:04 +0100

    adds basic cart page

---
+++ src/views/Cart.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <v-flex xs4 v-for='beer in beers' :key='beer.id'>
+        <Beer :beer='beer'/>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+import Beer from '@/components/Beer.vue'
+import { mapState } from 'vuex' 
+
+export default {
+  name: 'Beers',
+  components: {
+    Beer
+  },
+  computed: mapState(['beers']),
+}
+</script>
