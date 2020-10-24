faaee1979767c0010689e5022e060782b94e8ca3
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-25 00:10:28 +0100

    fixes issue footer not visible on products view

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
       <router-view/>  
     </v-main> 
 
-    <v-footer>
+    <v-footer fixed>
       This is a non-commercial website developed as part of coursework at the University of the Highlands and Islands, to allow student teams to experiment with building websites based on open APIs
     </v-footer>
 
