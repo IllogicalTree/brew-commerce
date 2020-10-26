58c625e7f3cb76b03f34d801ee97691f54a8226f
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-26 22:23:18 +0000

    adds beer page

---
+++ src/views/Beer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <h1>{{beer.id}} - {{beer.name}}</h1>
+    <h2>{{beer.tagline}}</h2>
+    <p>{{beer.description}}</p>
+    <br>
+    <p>{{beer}}</p>
+
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+        beer: []
+    }
+  },
+  mounted() {
+    this.beer = this.$store.state.beers.find(beer => beer.id == this.$route.params.id)
+  }
+}
+</script>
