8b439cb4a4a560000f14ee6234989ae15652065a
MarcinWaskowiak <15010786@uhi.ac.uk>
2020-10-26 20:13:22 +0000

    Changed color of add to cart in products to green

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -5,7 +5,7 @@
                 <b>{{beer.id}} - {{beer.name}}</b>
                 <p>{{beer.tagline}}</p>
                 <b>{{beer.abv}} %ABV</b> <br>
-                <v-btn id="addToCart" medium outlined color="orange" dark @click="addToCart(beer, 1)">Add to cart </v-btn>
+                <v-btn id="addToCart" medium outlined color="rgb(88, 245, 91)" dark @click="addToCart(beer, 1)">Add to cart </v-btn>
                 <v-btn id="removeFromCart" medium outlined color="orange" dark @click="removeFromCart(beer, 1)">Remove 1 from cart </v-btn>
             </v-col>
             <v-col style='background: rgb(21,22,22)' justify="center">
