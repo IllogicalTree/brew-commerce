de4d818e6ee3f73677174dbe1eb47d43f40c29fc
MarcinWaskowiak <15010786@uhi.ac.uk>
2020-10-26 20:32:16 +0000

    Added temporally spacing around ABV in products for neater design

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -4,7 +4,9 @@
             <v-col style='background: rgb(31, 32, 32)' justify="center">
                 <b style = 'color: rgb(217, 219, 217)'>{{beer.id}} - {{beer.name}}</b>
                 <p style = 'color: rgb(187, 191, 187)'>{{beer.tagline}}</p>
-                <b style = 'color: rgb(187, 191, 187)'>{{beer.abv}} %ABV</b> <br>
+                <b style = 'color: rgb(187, 191, 187)'>{{beer.abv}} %ABV</b> 
+                    <br>
+                    <br>
                 <v-btn id="addToCart" medium outlined color="rgb(88, 245, 91)" dark @click="addToCart(beer, 1)">Add to cart </v-btn>
                 <v-btn id="removeFromCart" medium outlined color="rgb(250, 105, 95)" dark @click="removeFromCart(beer, 1)">Remove 1 from cart </v-btn>
             </v-col>
