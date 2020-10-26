c8ba807e1fdc324d2a03256c361e8f342d02430f
CALUM MACINTOSH <20014543@uhi.ac.uk>
2020-10-26 10:03:05 +0000

    Add hero image component to home
    Add a hero image component and import in to Home.view

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hero-image">
+    <div class="hero-text">
+      <h1 class="display-3 font-weight-bold mb-2">Brew-Commerce</h1>
+      <h2 class="display-1 font-weight-bold mb-3">Beer glorious beer</h2>
+      <br />
+      <v-btn to="/beers" id="buy" x-large outlined color="success" dark
+        >Buy More Beer
+      </v-btn>
+      <p></p>
+      <v-btn to="/cart" id="view" x-large outlined color="orange" dark>View Cart </v-btn>
+    </div>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: "HeroImage",
+};
+</script>
+
+<style scoped>
+/* The hero image */
+>>>.hero-image {
+  /* Use "linear-gradient" to add a darken background effect to the image (photographer.jpg). This will make the text easier to read */
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("./../assets/heroimage.jpg");
+
+  /* Set a specific height */
+  height: 50%;
+
+  /* Position and center the image to scale nicely on all screens */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+/* Place text in the middle of the image */
+>>>.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,39 @@
+
 <template>
-  <div>
-    <h1>Home page</h1>
+<div class="home-container">
+  <div class="hero">
+    <HeroImage />
   </div>
+  <h1>Lots of beer here!</h1>
+  <p>Tasty, tasy beer.</p>
+</div>
 </template>
 
 <script>
+import HeroImage from "@/components/HeroImage.vue";
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  components: {
+    HeroImage,
+  },
+};
 </script>
+
+<style>
+.hero {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("./../assets/heroimage.jpg");
+
+  height: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.home-container {  
+  height: 100%;
+  width: 100%;
+position: absolute;
+
+}
+</style>
