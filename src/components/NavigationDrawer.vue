a54aac6e5945b9f79a3bc587f0846e22d895cc37
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-27 16:31:38 +0000

    adds navigation drawer

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+        <v-list 
+            nav 
+            dense
+        >
+            <v-btn to="/" text>
+            <span class="mr-2">Home</span>
+            <v-icon>mdi-home</v-icon>
+            </v-btn>
+
+            <v-btn to="/beers" text>
+            <span class="mr-2">Products</span>
+            <v-icon>mdi-beer</v-icon>
+            </v-btn>
+
+            <v-btn to="/cart" text>
+            <span class="mr-2">View Cart ({{cartItems}})</span>
+            <v-icon>mdi-cart-outline</v-icon>
+            </v-btn>
+        </v-list>
+
+    </v-navigation-drawer>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+    name: 'NavigationDrawer',
+    data: () => ({
+        drawer: false
+    }),
+    computed: mapState(['cartItems']),
+}
+</script>
+
+<style>
+
+</style>
