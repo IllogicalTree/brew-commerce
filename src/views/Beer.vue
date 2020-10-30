4a04ac4dcc91c151d50116018a1f0d482b8f95ea
JORDAN NEWLANDS <15027887@uhi.ac.uk>
2020-10-30 19:08:58 +0000

    displays error to use if beer fails to load

--- src/views/Beer.vue
+++ src/views/Beer.vue
@@ -1,36 +1,42 @@
 <template>
   <v-container>
-    <v-img class="beerimg" :src="beer.image_url"></v-img>
-    <h1>{{ beer.id }} - {{ beer.name }}</h1>
-    <h2>{{ beer.tagline }}</h2>
-    <h3>{{ beer.abv }} ABV</h3>
-    <br />
-    <p>{{ beer.description }}</p>
-    <br />
-    <div class="buttons">
-      <v-btn
-        id="addToCart"
-        medium
-        color="rgb(88, 245, 91)"
-        outlined
-        dark
-        @click="addToCart(beer, 1)"
-        >Add to cart
-      </v-btn>
-      <br />
-      <br />
-      <v-btn
-        id="removeFromCart"
-        medium
-        color="rgb(250, 105, 95)"
-        outlined
-        dark
-        @click="removeFromCart(beer, 1)"
-        >Remove 1 from cart
-      </v-btn>
-      <br />
-      <br />
+    <div v-if='!beer'>
+      <h1>Error occured loading beer details, check out our beers page.</h1>
       <v-btn @click="$router.go(-1)">Go back</v-btn>
+    </div>
+    <div v-if='beer'>
+      <v-img class="beerimg" :src="beer.image_url"></v-img>
+      <h1>{{ beer.id }} - {{ beer.name }}</h1>
+      <h2>{{ beer.tagline }}</h2>
+      <h3>{{ beer.abv }} ABV</h3>
+      <br />
+      <p>{{ beer.description }}</p>
+      <br />
+      <div class="buttons">
+        <v-btn
+          id="addToCart"
+          medium
+          color="rgb(88, 245, 91)"
+          outlined
+          dark
+          @click="addToCart(beer, 1)"
+          >Add to cart
+        </v-btn>
+        <br />
+        <br />
+        <v-btn
+          id="removeFromCart"
+          medium
+          color="rgb(250, 105, 95)"
+          outlined
+          dark
+          @click="removeFromCart(beer, 1)"
+          >Remove 1 from cart
+        </v-btn>
+        <br />
+        <br />
+        <v-btn @click="$router.go(-1)">Go back</v-btn>
+      </div>
     </div>
   </v-container>
 </template>
